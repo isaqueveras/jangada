53b04851e16886cb7ee4186aba6b0496cb603667
Isaque Veras <isaque.verasccb@gmail.com>
2025-08-22 00:18:25 -0300

    feat: add template of base page

---
+++ internal/cli/tmpl/new-app/tmpl_web_layouts_page.go
@@ -0,0 +1,28 @@
+package newapp
+
+const tmplWebLayoutsPage TmplNewProject = `package layouts
+
+templ Page(title string) {
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>{title} - Jangada Framework</title>
+      <link rel="icon" type="image/png" href="public/background.png" />
+      <script src="https://unpkg.com/htmx.org"></script>
+      <script src="https://cdn.tailwindcss.com"></script>
+			<style>
+        body {
+          background-image: url("public/background.png");
+          background-repeat: repeat;
+          background-size: 220px 220px;
+          background-position: top left;
+        }
+      </style>
+    </head>
+    <body>
+      { children... }
+    </body>
+  </html>
+}
+`
