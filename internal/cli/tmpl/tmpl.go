a4122f3ef3cb743d1760827506dd58db12fa6f18
Isaque Veras <isaque.verasccb@gmail.com>
2025-08-24 14:01:03 -0300

    feat: add functions to get templates

--- internal/cli/tmpl/tmpl.go
+++ internal/cli/tmpl/tmpl.go
@@ -2,7 +2,35 @@ package tmpl
 
 import newapp "github.com/isaqueveras/jangada/internal/cli/tmpl/new-app"
 
-// GetTemplateForNewProject returns a map of templates for new project
-func GetTemplateForNewProject() map[string]newapp.TmplNewProject {
-	return newapp.TemplatesNewProject
-}
+// GetTemplateForNewApp returns a map of templates for new app
+func GetTemplateForNewApp() map[string]string { return newapp.Template }
+
+// GetTemplateForNewWorker returns a map of templates for new worker
+func GetTemplateForNewWorker() map[string]string { return nil }
+
+// GetTemplateForNewConsole returns a map of templates for new console
+func GetTemplateForNewConsole() map[string]string { return nil }
+
+// GetTemplateForNewJob returns a map of templates for new job
+func GetTemplateForNewJob() map[string]string { return nil }
+
+// GetTemplateForNewApplicationLayer returns a map of templates for new application layer
+func GetTemplateForNewApplicationLayer() map[string]string { return nil }
+
+// GetTemplateForNewDomainLayer returns a map of templates for new domain layer
+func GetTemplateForNewDomainLayer() map[string]string { return nil }
+
+// GetTemplateForNewInfrastructureLayer returns a map of templates for new infrastructure layer
+func GetTemplateForNewInfrastructureLayer() map[string]string { return nil }
+
+// GetTemplateForNewInterfaceLayer returns a map of templates for new interface layer
+func GetTemplateForNewInterfaceLayer() map[string]string { return nil }
+
+// GetTemplateForNewTest returns a map of templates for new test
+func GetTemplateForNewTest() map[string]string { return nil }
+
+// GetTemplateForAddPackage returns a map of templates for add package
+func GetTemplateForAddPackage() map[string]string { return nil }
+
+// GetTemplateForNewPlugin returns a map of templates for new plugin
+func GetTemplateForNewPlugin() map[string]string { return nil }
