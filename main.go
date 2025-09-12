6d75d6104d6ffd849e69cd642ca9bdeba2f9e6d3
Isaque Veras <isaque.verasccb@gmail.com>
2025-09-12 18:30:27 -0300

    fix: init cli application

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
-	"log"
+	"path/filepath"
 
 	cli "github.com/isaqueveras/jangada/internal"
+	newapp "github.com/isaqueveras/jangada/internal/new-app"
+	"github.com/isaqueveras/jangada/internal/sail"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	if err := cli.New(); err != nil {
-		log.Println(err.Error())
+	dirBase, _ := filepath.Abs("")
+
+	cli.Init(dirBase)
+	cli.SetFullDirectoryPath()
+
+	cmd := &cobra.Command{
+		Use:     "jangada",
+		Short:   "Jangada is a CLI tool for project scaffolding and code generation.",
+		Example: "jangada new my-app --module github.com/username/my-app --database postgres",
+	}
+
+	cmd.AddCommand(
+		// Command `jangada new my-app`
+		newapp.Command(),
+
+		// Command `jangada sail ...`
+		sail.Command(),
+	)
+
+	if err := cmd.Execute(); err != nil {
+		panic(err)
 	}
 }
