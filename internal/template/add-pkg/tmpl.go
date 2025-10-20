29904bea5f9350bbd633cd871245ff68757fcf64
Isaque Véras <isaque.verasccb@gmail.com>
2025-10-19 22:19:11 -0300

    feat: init package map template

---
+++ internal/template/add-pkg/tmpl.go
@@ -0,0 +1,17 @@
+package addpkg
+
+var TemplatePackage = map[string]string{
+	"pkg/jang-cache/cache.go":         "cache",
+	"pkg/jang-cron/cron.go":           "cron",
+	"pkg/jang-event/event.go":         "event",
+	"pkg/jang-hook/hook.go":           "hook",
+	"pkg/jang-flag/flag.go":           "flag",
+	"pkg/jang-worker/worker.go":       "worker",
+	"pkg/jang-metric/metric.go":       "metric",
+	"pkg/jang-metadata/metadata.go":   "metadata",
+	"pkg/jang-signals/signals.go":     "signals",
+	"pkg/jang-singleton/singleton.go": "singleton",
+	"pkg/jang-redis/redis.go":         "redis",
+	"pkg/jang-postgress/postgres.go":  "postgres",
+	"pkg/jang-sqlite/sqlite.go":       "sqlite",
+}
