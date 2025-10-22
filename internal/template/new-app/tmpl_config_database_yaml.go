93e38fc7f7142a84c2e53230846759e1fd4cd1f5
Isaque Véras <isaque.verasccb@gmail.com>
2025-10-21 21:23:07 -0300

    fix: using space

--- internal/template/new-app/tmpl_config_database_yaml.go
+++ internal/template/new-app/tmpl_config_database_yaml.go
@@ -19,38 +19,38 @@ const tmplConfigDatabaseYAML = `development:
       path_ca: /etc/ssl/certs/ca.crt
 
 test:
-	- nick: default
-		name: {{ .AppName }}
-		username: postgres
-		password: postgres
-		hostname: localhost
-		port: "5432"
-		max_conn: 20
-		max_idle: 5
-		read_only: false
-		main: true
-		transaction_timeout: 30
-		ssl_mode: disable
-		ssl_client:
-			path_cert: /etc/ssl/certs/client.crt
-			path_key: /etc/ssl/private/client.key
-			path_ca: /etc/ssl/certs/ca.crt
+  - nick: default
+    name: {{ .AppName }}
+    username: postgres
+    password: postgres
+    hostname: localhost
+    port: "5432"
+    max_conn: 20
+    max_idle: 5
+    read_only: false
+    main: true
+    transaction_timeout: 30
+    ssl_mode: disable
+    ssl_client:
+      path_cert: /etc/ssl/certs/client.crt
+      path_key: /etc/ssl/private/client.key
+      path_ca: /etc/ssl/certs/ca.crt
 
 production:
-	- nick: default
-		name: {{ .AppName }}
-		username: postgres
-		password: postgres
-		hostname: localhost
-		port: "5432"
-		max_conn: 20
-		max_idle: 5
-		read_only: false
-		main: true
-		transaction_timeout: 30
-		ssl_mode: enable
-		ssl_client:
-			path_cert: /etc/ssl/certs/client.crt
-			path_key: /etc/ssl/private/client.key
-			path_ca: /etc/ssl/certs/ca.crt
+  - nick: default
+    name: {{ .AppName }}
+    username: postgres
+    password: postgres
+    hostname: localhost
+    port: "5432"
+    max_conn: 20
+    max_idle: 5
+    read_only: false
+    main: true
+    transaction_timeout: 30
+    ssl_mode: enable
+    ssl_client:
+      path_cert: /etc/ssl/certs/client.crt
+      path_key: /etc/ssl/private/client.key
+      path_ca: /etc/ssl/certs/ca.crt
 `
