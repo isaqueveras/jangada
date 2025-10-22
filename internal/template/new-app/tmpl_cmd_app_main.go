9e2febd66821d1de9c06f7aa4582ccb044964c2e
Isaque Véras <isaque.verasccb@gmail.com>
2025-10-21 21:33:04 -0300

    feat: init connections pool in main.go

--- internal/template/new-app/tmpl_cmd_app_main.go
+++ internal/template/new-app/tmpl_cmd_app_main.go
@@ -2,9 +2,16 @@ package newapp
 
 const tmplAppMain = `package main
 
-import "{{ .ModuleName }}/core"
+import (
+	"{{ .ModuleName }}/core"
+	"{{ .ModuleName }}/services/database"
+)
 
 func main() {
 	server := core.New()
+
+	conn := database.NewConnectionPool(server.Config().GetDatabases()...)
+	defer conn.CloseConnections()
+
 	server.Init()
 }`
