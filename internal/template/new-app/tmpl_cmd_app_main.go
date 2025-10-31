ca87707a193be05a1b4d1664146b228fd7716c0b
Isaque Véras <isaque.verasccb@gmail.com>
2025-10-30 21:53:15 -0300

    feat: create builder file in app, domain & infra.

--- internal/template/new-app/tmpl_cmd_app_main.go
+++ internal/template/new-app/tmpl_cmd_app_main.go
@@ -16,8 +16,49 @@ func main() {
 	defer conn.CloseConnections()
 
 	// Uncomment the code below to enable the transport layer
-	// transport.Handler(server)
+	// transport.Handler(server, conn)
 
 	server.Init()
+}
+`
+
+const tmplApplicationBuilder = `// Package application provides application management
+package application
+
+import (
+	"{{ .ModuleName }}/internal/domain"
+	"{{ .ModuleName }}/internal/infrastructure"
+)
+
+type builder struct {
+	repository domain.RepositoryBuilder
+}
+
+// New creates a new instance of builder with the given repository
+func New() *builder {
+	return &builder{
+		repository: infrastructure.New(),
+	}
+}
+`
+
+const tmplDomainBuilder = `// Package domain defines the interfaces for building repositories and services.
+package domain
+
+// RepositoryBuilder is a builder for repository
+type RepositoryBuilder interface{}
+
+// ServiceBuilder is a builder for service
+type ServiceBuilder interface{}
+`
+
+const tmplInfrastructureBuilder = `// Package infrastructure provides infrastructure services and builders
+package infrastructure
+
+type builder struct{}
+
+// New creates a new instance of builder for infrastructure
+func New() *builder {
+	return &builder{}
 }
 `
