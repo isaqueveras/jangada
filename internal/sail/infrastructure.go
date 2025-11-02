fff93357933cf36b9164f2ca90eb8eb6cd8b0e21
Isaque Véras <isaque.verasccb@gmail.com>
2025-11-02 15:22:36 -0300

    feat: add alias to commands & add setup to command create infra layer

---
+++ internal/sail/infrastructure.go
@@ -0,0 +1,12 @@
+package sail
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// Infrastructure is the handler for the 'sail infrastructure' command.
+func Infrastructure(cmd *cobra.Command, args []string) {
+	log.Fatal("Infrastructure layer not implemented yet")
+}
