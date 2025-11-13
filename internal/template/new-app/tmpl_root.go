75a2bdfd7ff6fdfb1a828f6ea1e29a4203aca77f
Isaque Véras <isaque.verasccb@gmail.com>
2025-11-13 19:49:58 -0300

    feat: add template for .env and .air

--- internal/template/new-app/tmpl_root.go
+++ internal/template/new-app/tmpl_root.go
@@ -79,10 +79,93 @@ lint:
 `
 
 const tmplGitIgnore string = `db/*.db
-db/*.db-*
-
 bin/
 tmp/
 
 *_templ.go
-*_templ.txt`
+.env`
+
+const tmplEnv string = `# ================== APPICATION ==================
+
+# ENVIRONMENT -> define the environment used during the application
+ENVIRONMENT=development # development|testing|production
+
+# APP_NAME -> defines the system name
+APP_NAME={{ ToLower .AppName }}
+
+# APP_DESCRIPTION -> defines the system description
+APP_DESCRIPTION="description of the application"
+
+# APP_ADDRESS -> defines the address where the application will be executed
+APP_ADDRESS={{ .DefaultHost }}
+
+# APP_VERSION -> defines the version of the application that is running
+APP_VERSION=v0.0.0
+
+# APP_DEBUG -> defines whether the application will run in debug mode
+APP_DEBUG=true
+
+# PROMETHEUS_PUSHGATEWAY -> defines the application address for Prometheus Pushgateway
+PROMETHEUS_PUSHGATEWAY=http://localhost:9091
+
+# ================== DATABASE ==================
+
+# {{ ToUpper .AppName }}_DATABASE_NICK -> defines the alias for the database connection
+{{ ToUpper .AppName }}_DATABASE_NICK={{ .AppName }}
+
+# {{ ToUpper .AppName }}_DATABASE_NAME -> defines the database name
+{{ ToUpper .AppName }}_DATABASE_NAME={{ ToLower .AppName }}
+
+# {{ ToUpper .AppName }}_DATABASE_USER -> defines the database username
+{{ ToUpper .AppName }}_DATABASE_USER=postgres
+
+# {{ ToUpper .AppName }}_DATABASE_PASS -> define the database user's password
+{{ ToUpper .AppName }}_DATABASE_PASS=postgres
+
+# {{ ToUpper .AppName }}_DATABASE_HOST -> defines the database address
+{{ ToUpper .AppName }}_DATABASE_HOST=localhost
+
+# {{ ToUpper .AppName }}_DATABASE_PORT -> defines the database port
+{{ ToUpper .AppName }}_DATABASE_PORT=5432
+
+# {{ ToUpper .AppName }}_DATABASE_MAX_CONN -> defines the maximum number of connections the database will have access to
+{{ ToUpper .AppName }}_DATABASE_MAX_CONN=20
+
+# {{ ToUpper .AppName }}_DATABASE_MAX_IDLE -> defines the maximum time a connection can wait
+{{ ToUpper .AppName }}_DATABASE_MAX_IDLE=5
+
+# {{ ToUpper .AppName }}_DATABASE_READ_ONLY -> defines whether the database is read-only or write-only
+{{ ToUpper .AppName }}_DATABASE_READ_ONLY=false
+
+# {{ ToUpper .AppName }}_DATABASE_MAIN -> defines whether the database is the primary one or not
+{{ ToUpper .AppName }}_DATABASE_MAIN=true
+
+# {{ ToUpper .AppName }}_DATABASE_TIMEOUT -> defines the timeout that a connection can have in the database
+{{ ToUpper .AppName }}_DATABASE_TIMEOUT=30
+
+# {{ ToUpper .AppName }}_DATABASE_SSL_MODE -> defines which security mode the database will be used
+{{ ToUpper .AppName }}_DATABASE_SSL_MODE=disable
+
+# {{ ToUpper .AppName }}_DATABASE_SSL_CERT -> defines the address for SSL certification
+{{ ToUpper .AppName }}_DATABASE_SSL_CERT=/etc/ssl/certs/client.crt
+
+# {{ ToUpper .AppName }}_DATABASE_SSL_KEY -> defines the address for SSL certification
+{{ ToUpper .AppName }}_DATABASE_SSL_KEY=/etc/ssl/private/client.key
+
+# {{ ToUpper .AppName }}_DATABASE_SSL_CA -> defines the address for SSL certification
+{{ ToUpper .AppName }}_DATABASE_SSL_CA=/etc/ssl/certs/ca.crt
+`
+
+const tmplAirToml = `root = "."
+tmp_dir = "tmp"
+
+[build]
+  cmd = "go build -o ./bin/{{ ToLower .AppName }}-app ./cmd/app"
+  bin = "tmp/main"
+  full_bin = "./bin/{{ ToLower .AppName }}-app"
+  include_ext = ["go", "tmpl"]
+  exclude_dir = ["vendor", "tmp"]
+
+[log]
+  level = "info"
+  color = true`
