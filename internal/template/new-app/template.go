e2552a8ce965704cb0dbe5a041e3b0de2f42d957
Isaque Véras <isaque.verasccb@gmail.com>
2025-11-29 14:21:30 -0300

    fix: path files

--- internal/template/new-app/template.go
+++ internal/template/new-app/template.go
@@ -3,13 +3,17 @@ package newapp
 
 // Template is a map of templates for new app
 var Template = map[string]string{
-	"go.mod":       tmplGoMod,     // contains module of the app
-	".air.toml":    tmplAirToml,   // contains air.toml of the app
-	".gitignore":   tmplGitIgnore, // contains gitignore of the app
-	".env":         tmplEnv,       // contains env of the app
-	".env.example": tmplEnv,       // contains env of example of the app
-	"Makefile":     tmplMakefile,  // contains makefile of the app
-	"README.md":    tmplReadme,    // contains readme of the app
+	"go.mod":             tmplGoMod,         // contains module of the app
+	".air.toml":          tmplAirToml,       // contains air.toml of the app
+	".gitignore":         tmplGitIgnore,     // contains gitignore of the app
+	".env":               tmplEnv,           // contains env of the app
+	".env.example":       tmplEnv,           // contains env of example of the app
+	"Makefile":           tmplMakefile,      // contains makefile of the app
+	"README.md":          tmplReadme,        // contains readme of the app
+	"Dockerfile.app":     tmplDockerfile,    // contains dockerfile of the app
+	"prometheus.yml":     tmplPrometheus,    // contains prometheus config of the app
+	"docker-compose.yml": tmplDockerCompose, // contains docker compose of the app
+	"database.sql":       "",                // contains database seed of the app
 
 	"core/core.go": tmplCoreCore, // contains core of the app
 
@@ -24,19 +28,14 @@ var Template = map[string]string{
 	"pkg/database/seeds/seeds.go":       serviceDatabaseSeedsTemp,
 	"pkg/database/migrations/.keep":     "",
 
-	"docker/Dockerfile":         tmplDockerfile,
-	"docker/prometheus.yml":     tmplPrometheus,
-	"docker/docker-compose.yml": tmplDockerCompose,
-
 	"cmd/app/main.go": tmplAppMain, // contains main of the app
 
 	"public/.keep": "", // contains public files of the app
 
-	"web/assets/css/input.css":  "", // contains input css of the app
-	"web/assets/css/output.css": "", // contains output css of the app
-	"web/assets/js/.keep":       "", // contains js files of the app
-	"web/components/.keep":      "", // contains components of the app
-
+	"web/assets/css/input.css":    "",                     // contains input css of the app
+	"web/assets/css/output.css":   "",                     // contains output css of the app
+	"web/assets/js/.keep":         "",                     // contains js files of the app
+	"web/components/.keep":        "",                     // contains components of the app
 	"web/layouts/welcome.templ":   tmplWebLayoutsWelcome,  // contains welcome page of the app
 	"web/layouts/page.templ":      tmplWebLayoutsPage,     // contains page of the app
 	"web/layouts/not_found.templ": tmplWebLayoutsNotFound, // contains not found page of the app
